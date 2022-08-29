a17c63e2c9e5693f4c7b6904fb9b752cf3b112f0
IDN-Maximillien <111720429+IDN-Maximillien@users.noreply.github.com>
2022-08-29 10:34:38 +0700

    Update Binary-Search.py

--- Binary-Search.py
+++ Binary-Search.py
@@ -1,1 +1,38 @@
 
+def binSearch(angka, daftar):
+    daftar.sort()
+    langkah = 0
+    ketemu = False
+    awal = 0
+    akhir = len(daftar)-1
+
+    while awal <= akhir and not ketemu:
+        tengah = (awal+akhir)//2
+        print(f'Awal = {awal}, Tengah = {tengah}, Akhir = {akhir}')
+        if daftar[tengah] == angka:
+            ketemu = True
+            break
+        elif angka > daftar[tengah]:
+            awal = tengah + 1
+        else:
+            akhir = tengah - 1
+        langkah += 1
+        
+    if ketemu == True:
+        print(f'Angka {angka} ditemukan pada posisi ke {awal +1}!')
+    else:
+        print(f'Angka {angka} tidak ditemukan! Pencarian dilakukan sebanyak {langkah} langkah.')
+
+def searchAlt(angka, listAngka):
+    for i in range(len(listAngka)):
+        if listAngka[i] == angka:
+            stat = True
+        else:
+            continue
+    print(f'{angka} ditemukan pada indeks ke {i+1}')
+
+listAngka = [67,13,71,39,45]
+cariangka = int(input('Angka : '))    
+stat = False
+
+binSearch(cariangka, listAngka)
