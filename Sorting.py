7927698fab9092b42269c6e052bd30960d1ac629
IDN-Maximillien <111720429+IDN-Maximillien@users.noreply.github.com>
2022-08-31 12:15:55 +0700

    Update Sorting.py

--- Sorting.py
+++ Sorting.py
@@ -1,12 +1,26 @@
-data = [67,13,71,39,45]
+# data = [67,13,71,39,45]
 
 def insert_sort(L):
+    print("Sebelum sort : ",L)
+
+    # for j in range(1, len(L) + 1):
     for i in range(1, len(L)):
-        kanan = int(L[i])
+        kanan = L[i]
         kiri = i-1
         while kiri >= 0 and L[kiri] > kanan:
             L[kiri + 1] = L[kiri]
             kiri -= 1
-    print('Hello', L)
+        L[kiri + 1] = kanan
+    
+        
+    print("Sesudah sort : ",L)
+    # print('Hello', L)
+
+data = []
+
+panjang = int(input('Panjang List : '))
+for j in range(1, panjang+1):
+    angka = int(input(f'Angka {j} : '))
+    data.append(angka)
 
 insert_sort(data)
